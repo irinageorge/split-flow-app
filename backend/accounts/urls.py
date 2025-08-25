c34ce4acaa30b690b03069b10e3f5aaa79d6ab0b
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-08-25 12:25:24 +0300

    ULRS fixed - separated for the API

---
+++ backend/accounts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.account_list, name='account-list'),
+    path('<int:pk>/', views.account_detail, name='account-detail'),
+]
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,10 +1,7 @@
-# backend/urls.py
 from django.contrib import admin
-from django.urls import path
-from accounts.views import account_list, account_detail
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', account_list),             # /api/accounts/ returns all
-    path('accounts/<int:pk>/', account_detail), # /api/accounts/1/ returns single
+    path('accounts/', include('accounts.urls')),
 ]
