018b644dcd98483afb86a9e4c01d0b03c1cbb0af
Irina <irina17g@gmail.com>
2025-08-25 13:17:10 +0300

    adding google font

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,12 @@ function App() {
   return (
     <div className="center">
       <div className="title-row">
-        <h1 className="title">Welcome to SplitFlow</h1>
+        <h1 className="title font-lilita">Welcome to SplitFlow</h1>
         <img src={appIcon} alt="app icon" className="app-icon" />
       </div>
+      <p className="subtitle font-lilita">
+        Effortlessly split bills, track expenses and stay connected with friends.
+      </p>
       <div className="button-row">
         <SignIn />
         <SignUp />
