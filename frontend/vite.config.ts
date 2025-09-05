d6fb61560feee7115929f3ff0ac8f6073eefbcb8
Irina <irina17g@gmail.com>
2025-09-05 13:22:11 +0300

    Adding Tanstack query and making the first request

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,12 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
+import path from "path"
 
-// https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 5000
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
