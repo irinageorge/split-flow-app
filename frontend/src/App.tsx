bae4d0a0ae31c86d509bb0c8ca3add6a488eb1ae
Irina Georgieva <114845971+irinageorge@users.noreply.github.com>
2025-09-05 13:22:33 +0300

    Adding Tanstack query and making the first request (#4)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,41 @@
 import './App.css'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 import appIcon from "./assets/split.svg"
 import SignIn from './componentsUI/SignIn'
 import SignUp from './componentsUI/SignUp'
+import { useHomePage } from './services/HomePageQuery'
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <div className="center">
-      <div className="title-row">
-        <h1 className="title font-lilita">Welcome to SplitFlow</h1>
-        <img src={appIcon} alt="app icon" className="app-icon" />
-      </div>
-      <p className="subtitle font-lilita">
-        Effortlessly split bills, track expenses and stay connected with friends.
-      </p>
-      <div className="button-row">
-        <SignIn />
-        <SignUp />
-      </div>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Home />
+    </QueryClientProvider>
   )
+}
+
+function Home() {
+
+  const {data} = useHomePage();
+  console.log("Data:", data);
+
+  return <div className="center">
+    <div className="title-row">
+      <h1 className="title font-lilita">Welcome to SplitFlow</h1>
+      <img src={appIcon} alt="app icon" className="app-icon" />
+    </div>
+    <p className="subtitle font-lilita">
+      Effortlessly split bills, track expenses and stay connected with friends.
+    </p>
+    <div className="button-row">
+      <SignIn />
+      <SignUp />
+    </div>
+  </div>
 }
 
 export default App
---
+++ frontend/src/services/HomePageQuery.ts
@@ -0,0 +1,28 @@
+import { useQuery, keepPreviousData } from "@tanstack/react-query";
+
+export const useHomePage = () => {
+  const { isPending, isError, data, error } = useQuery({
+    queryKey: ["login"],
+    queryFn: async () => {
+      const response = await fetch("http://127.0.0.1:8000/accounts/login/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          password: "stlstlstl",
+          email: "stl@gmail.scom",
+        }),
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return await response.json();
+    },
+    placeholderData: keepPreviousData,
+    refetchOnWindowFocus: false,
+    // enabled: true,
+  });
+
+  return { isPending, isError, data, error };
+};
