697745383efda3e2e1c2e68bad01c8741bd56c6e
Irina <irina17g@gmail.com>
2025-09-06 23:21:04 +0300

    adding responsive design

--- frontend/src/componentsUI/LoginPage.tsx
+++ frontend/src/componentsUI/LoginPage.tsx
@@ -17,8 +17,7 @@ export const LoginPage = () => {
             <h1 className="title">SplitFlow</h1>
         </div>
         <p className="subtitle">
-            Effortlessly split bills,track expenses <br />
-            and stay connected with friends.
+            Effortlessly split bills,track expenses and stay connected with friends.
         </p>
         <div className="button-row">
             <SignIn />
