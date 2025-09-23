c6ada16493f64db88748ee3f3f38c9ff6da9fa58
StiliyanEnchev <145556929+StiliyanEnchev@users.noreply.github.com>
2025-09-23 17:48:05 +0300

    Add bills api (#3)
    - Initialized SplitFlow: added Bills and BillEntry models
    - Added BillUser relation for many-to-many association
    - Removed User from import

--- backend/splitflow/models.py
+++ backend/splitflow/models.py
@@ -22,3 +22,9 @@ class BillEntry(models.Model):
 
     def __str__(self):
         return f'{self.user.username} - {self.amount}'
+
+class BillUser(models.Model):
+    bill = models.ForeignKey(Bill, on_delete=models.CASCADE)
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+    joined_on = models.DateTimeField(auto_now_add=True)
+
