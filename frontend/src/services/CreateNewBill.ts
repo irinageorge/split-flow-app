cae5c21864dd682d76ea1cdb6294d5461244c856
Irina Georgieva <114845971+irinageorge@users.noreply.github.com>
2025-09-26 19:06:40 +0300

    Fixing FE bugs (#13)

--- frontend/src/services/CreateNewBill.ts
+++ frontend/src/services/CreateNewBill.ts
@@ -20,7 +20,7 @@ export function useCreateNewBill() {
       title,
       account_id,
     }: CreateRequest): Promise<CreateResponse> => {
-      const res = await fetch("http://127.0.0.1:8000/splitflow/bills", {
+      const res = await fetch("http://127.0.0.1:8000/splitflow/bills/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
